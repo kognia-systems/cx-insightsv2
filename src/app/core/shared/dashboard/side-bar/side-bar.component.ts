4dadc50aeeaffc04281832ae79132cdd582f289b
María Virginia Hernández Reyes <mirxtrem.apps@gmail.com>
2025-04-19 11:42:03 -0500

    remove collapsable in sidebar menu

--- src/app/core/shared/dashboard/side-bar/side-bar.component.ts
+++ src/app/core/shared/dashboard/side-bar/side-bar.component.ts
@@ -1,15 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [
-    RouterModule,
-  ],
+  imports: [CommonModule, RouterModule],
   templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.scss'
+  styleUrl: './side-bar.component.scss',
 })
-export class SideBarComponent {
-
-}
+export class SideBarComponent {}
